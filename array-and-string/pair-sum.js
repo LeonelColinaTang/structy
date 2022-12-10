4260945e91edcf742fe1be72f3d833082bf8b938
leonelcolina <tang.leonel@gmail.com>
2022-12-09 23:32:42 -0500

    Solved pair-sum

---
+++ array-and-string/pair-sum.js
@@ -0,0 +1,25 @@
+// Write a function, pairSum, that takes in an array and a target sum as arguments.
+// The function should return an array containing a pair of indices whose elements sum to the given target.
+// The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+const pairSum = (numbers, targetSum) => {
+    let result = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === targetSum) {
+                result.push(i, j);
+            }
+        }
+    }
+    return result;
+};
+pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
+pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
+pairSum([4, 7, 9, 2, 5, 1], 3); // -> [3, 5]
+pairSum([1, 6, 7, 2], 13); // -> [1, 2]
+pairSum([9, 9], 18); // -> [0, 1]
