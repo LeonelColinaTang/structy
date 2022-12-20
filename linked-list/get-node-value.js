71ed58552acf4c9be6348952f516704d5b8f24e7
leonelcolina <tang.leonel@gmail.com>
2022-12-19 23:34:16 -0500

    fix getNodeValue

---
+++ linked-list/get-node-value.js
@@ -0,0 +1,71 @@
+// Write a function, getNodeValue, that takes in the head of a linked list and an index.
+// The function should return the value of the linked list at the specified index.
+
+// If there is no node at the given index, then return null.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const getNodeValue = (head, index) => {
+
+    for (let i = 0; i !== index; i++) {
+        if (!head) {
+            return head;
+        } else {
+            head = head.next;
+        }
+    }
+    return head.val;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(getNodeValue(a, 2)); // 'c'
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+
+// // a -> b -> c -> d
+
+// getNodeValue(a, 3); // 'd'
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+
+// // a -> b -> c -> d
+
+// getNodeValue(a, 7); // null
+
+// const node1 = new Node("banana");
+// const node2 = new Node("mango");
+
+// node1.next = node2;
+
+// // banana -> mango
+
+// getNodeValue(node1, 1); // 'mango'
